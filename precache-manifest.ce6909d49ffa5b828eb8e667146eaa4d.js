ad86bc18bc9837c2ad7966c914b0fce7aa8c0f90
Shyam Gupta <shyamsundar.g@kiprosh.com>
2020-03-15 14:47:55 +0530

    Updates

---
+++ precache-manifest.ce6909d49ffa5b828eb8e667146eaa4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755eadcaf75dbc776712228f1084ebf7",
+    "url": "/react-datamaps-india/index.html"
+  },
+  {
+    "revision": "34f0abad32dac0aa38be",
+    "url": "/react-datamaps-india/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "515b6c54e79de48edcdb",
+    "url": "/react-datamaps-india/static/js/2.d7e03b14.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-datamaps-india/static/js/2.d7e03b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f0abad32dac0aa38be",
+    "url": "/react-datamaps-india/static/js/main.f3b0e698.chunk.js"
+  },
+  {
+    "revision": "dab1055056546f6623ec",
+    "url": "/react-datamaps-india/static/js/runtime-main.bbc8e664.js"
+  }
+]);
